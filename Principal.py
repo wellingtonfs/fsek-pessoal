54fcde61f662b4e5ed717101bd6c5945223f274a
wellingtonfs <wellingtonsilveira99@gmail.com>
2019-09-17 19:01:22 -0300

    ff

--- Principal.py
+++ Principal.py
@@ -258,6 +258,40 @@ def AchouCano():
             print(str(tempo_g[0]))
             time.sleep(10)
 
+def scan_gasoduto():
+    tempo_inicio, tempos_pista, anterior_leitura = -1, [], 0
+    tempo_dez, tempo_quinze, tempo_vinte = 0,0,0
+    #if alinhar(3) == 0:
+    '''m1.run_forever(speed_sp=150)
+    m2.run_forever(speed_sp=150)
+    time.sleep(2)
+    m1.stop(stop_action="brake")
+    m2.stop(stop_action="brake")
+    giraRobo(-90, 3)'''
+    while True:
+        m1.run_forever(speed_sp=150)
+        m2.run_forever(speed_sp=150)
+        if us.value() > 36 or us2.value() > 36:
+            m1.stop_action("brake")
+            m2.stop_action("brake")
+            giraRobo(180)
+            if len(tempos_pista) > 1:
+                tempos_pista.append(time.time())
+        elif anterior_leitura == -1:
+            tempos_pista.append(time.time()) 
+            anterior_leitura = Comm.ir2_value
+        elif (Comm.ir2_value - anterior_leitura) > 20: #Descobre um vao
+            tempos_pista.append(time.time())
+            anterior_leitura = Comm.ir2_value
+            if tempo_inicio == 0:
+                tempo_inicio = time.time()
+        elif (Comm.ir2_value - anterior_leitura) < 20: #Vao fechou
+            tempos_pista.append(time.time())
+            anterior_leitura = Comm.ir2_value
+            if (time.time() - tempo_inicio) > 0:
+                print(str(time.time() - tempo_inicio))
+                time.sleep(15)
+
 def alinhar(c): #Essa função alinha o lego a uma cor especifica c.
     if Sensor_Cor[0].value() == c and Sensor_Cor[1].value() == c:
         return 0
@@ -383,38 +417,7 @@ while True:
                 m2.stop(stop_action="brake")
                 time.sleep(10)
     elif Estado == 42:
-        tempo_inicio, tempo_iniciopista, anterior_leitura = 0, [], 0
-        tempo_dez, tempo_quinze, tempo_vinte = 0,0,0
-        #if alinhar(3) == 0:
-        m1.run_forever(speed_sp=150)
-        m2.run_forever(speed_sp=150)
-        time.sleep(2)
-        m1.stop(stop_action="brake")
-        m2.stop(stop_action="brake")
-        giraRobo(-90, 3)
-        while True:
-            m1.run_forever(speed_sp=150)
-            m2.run_forever(speed_sp=150)
-            if us.value() > 36 or us2.value() > 36:
-                m1.stop_action("brake")
-                m2.stop_action("brake")
-                giraRobo(180)
-                if len(tempo_iniciopista) > 1:
-                    tempo_iniciopista.append(time.time())
-            elif anterior_leitura == 0:
-                tempo_iniciopista.append(time.time()) 
-                anterior_leitura = Comm.ir2_value
-            elif (Comm.ir2_value - anterior_leitura) > 20: #Descobre um vao
-                tempo_iniciopista.append(time.time())
-                anterior_leitura = Comm.ir2_value
-                if tempo_inicio == 0:
-                    tempo_inicio = time.time()
-            elif (Comm.ir2_value - anterior_leitura) < 20: #Vao fechou
-                tempo_iniciopista.append(time.time())
-                anterior_leitura = Comm.ir2_value
-                if (time.time() - tempo_inicio) > 0:
-                    print(str(time.time() - tempo_inicio))
-                    time.sleep(15)
+        scan_gasoduto()
 '''
 
 cont = 0
--- Zandar.py
+++ Zandar.py
@@ -1,23 +1,61 @@
 #!/usr/bin/env python3
 from ev3dev.ev3 import *
-import time
+from threading import *
+import time, socket
 
 m1 = LargeMotor('outD')
 m2 = LargeMotor('outC')
+Estado = -1
 
-def giraRobo(graus, tempo = 2): #90 > 0: direita else: esquerda
-    razaoRobo = 5.25 / 3.0
-    if graus > 0:
-        m1.run_to_rel_pos(position_sp=(razaoRobo*graus),speed_sp=180,stop_action="brake")
-        m2.run_to_rel_pos(position_sp=-(razaoRobo*graus),speed_sp=180,stop_action="brake")
-    else:
-        m1.run_to_rel_pos(position_sp=-(razaoRobo*(graus*-1)),speed_sp=180,stop_action="brake")
-        m2.run_to_rel_pos(position_sp=(razaoRobo*(graus*-1)),speed_sp=180,stop_action="brake")
-    if tempo != 0:
-        time.sleep(tempo)
+arq = open("ll.txt", "w")
+arq.write("Leituras:\n\n")
+arq.close()
+
+def salvar(msg):
+    arq = open("ll.txt", "a")
+    arq.write(msg)
+    arq.close()
+    
+class Communication(Thread):
+    def __init__(self):
+        self.ir_value = 0
+        self.ir2_value = 0
+        Thread.__init__(self)
+
+    def run(self):
+        global Estado
+        while True:
+            try:
+                Cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                Cliente.bind(('169.255.168.150', 3561))
+                Cliente.listen(1)
+
+                while True:
+                    Msg, Endereco_Cliente = Cliente.accept()
+                    Dados = str(Msg.recv(1024).decode()).split(",")
+                    self.ir_value = int(Dados[0])
+                    self.ir2_value = int(Dados[1])
+                    if Estado == -1:
+                        print("Conectado")
+                        Estado = 0
+
+                Cliente.close()
+            except Exception as e:
+                print(e)
+                time.sleep(1)
+
+Comm = Communication()
+Comm.daemon = True
+Comm.start()
+
+temp = 0
 
 while True:
-    giraRobo(90)
-    giraRobo(-90)
-    giraRobo(180)
-    giraRobo(-180, 5)
+    if Estado == 0:
+        m1.run_forever(speed_sp=100)
+        m2.run_forever(speed_sp=100)
+
+        if (time.time() - temp) > 0.2:
+            str_p = "%d\n" %Comm.ir2_value
+            salvar(str_p)
+            temp = time.time()
