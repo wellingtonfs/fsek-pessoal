71aa852a76d067e49de70faeea905377e763ceed
Dyon16 <jardel.dyonisio@hotmail.com>
2019-10-04 18:59:04 -0300

    add

--- AgoraVai.py
+++ AgoraVai.py
@@ -9,17 +9,18 @@ m1 = LargeMotor('outD')
 m2 = LargeMotor('outC')
 # m3 = MediumMotor('outB')
 
-# cor = ColorSensor('in1') #2
-# cor2 = ColorSensor('in2') #4
-# us = UltrasonicSensor('in3')
+Sensor_Cor = [ColorSensor('in1'), ColorSensor('in2')]
+#Sensor_Cor[0] = ColorSensor('in1') #2
+#Sensor_Cor[1] = ColorSensor('in2') #4
+us = UltrasonicSensor('in3')
 # us2 = UltrasonicSensor('in4')
 # ir = InfraredSensor('in3')
 # ir2 = InfraredSensor('in1')
-tou = TouchSensor('in4')
+# tou = TouchSensor('in4')
 
-# cor.mode = 'COL-COLOR'
-# cor2.mode = 'COL-COLOR'
-# us.mode = 'US-DIST-CM'
+Sensor_Cor[0].mode = 'COL-COLOR'
+Sensor_Cor[1].mode = 'COL-COLOR'
+us.mode = 'US-DIST-CM'
 # us2.mode = 'US-DIST-CM'
 # ir.mode = 'IR-PROX'
 # ir2.mode = 'IR-PROX'
@@ -57,7 +58,7 @@ tou = TouchSensor('in4')
 #             if (time.time() - tempo_inicio) > 0:
 #                 print(str(time.time() - tempo_inicio))
 #                 time.sleep(15)
-well = False
+'''well = False
 
 while True:
     if tou.value() == True:
@@ -70,4 +71,97 @@ while True:
     if well:
         m1.run_forever(speed_sp=150)
         m2.run_forever(speed_sp=150)
-        time.sleep(1)
+        time.sleep(1)'''
+
+'''while True:
+    print ("%d" %us.value())'''
+
+def alinhar(c): #Essa função alinha o lego a uma cor especifica c.
+    if Sensor_Cor[0].value() == c and Sensor_Cor[1].value() == c:
+        return 0
+    while True:
+        if Sensor_Cor[0].value() == c:
+            m1.stop(stop_action="brake")
+            m2.stop(stop_action="brake")
+            while Sensor_Cor[0].value() == c:
+                m1.run_forever(speed_sp=-50)
+                m2.run_forever(speed_sp=-50)
+            m1.stop(stop_action="brake")
+            m2.stop(stop_action="brake")
+            #m1.run_forever(speed_sp=-150)
+            m2.run_forever(speed_sp=100)
+            while Sensor_Cor[1].value() != c:
+                if Sensor_Cor[1].value() == 0:
+                    return 1
+                if Sensor_Cor[0].value() != c:
+                    m2.stop(stop_action="brake")
+                    m1.run_forever(speed_sp=70)
+                else:
+                    m1.stop(stop_action="brake")
+                    m2.run_forever(speed_sp=100)
+                if 40 < us2.value() < 400:
+                    return 2
+            break
+
+        if Sensor_Cor[1].value() == c:
+            m1.stop(stop_action="brake")
+            m2.stop(stop_action="brake")
+            while Sensor_Cor[1].value() == c:
+                m1.run_forever(speed_sp=-50)
+                m2.run_forever(speed_sp=-50)
+            m1.stop(stop_action="brake")
+            m2.stop(stop_action="brake")
+            m1.run_forever(speed_sp=100)
+            #m2.run_forever(speed_sp=-150)
+            while Sensor_Cor[0].value() != c:
+                if Sensor_Cor[0].value() == 0:
+                    return 1
+                if Sensor_Cor[1].value() != c:
+                    m1.stop(stop_action="brake")
+                    m2.run_forever(speed_sp=70)
+                else:
+                    m1.run_forever(speed_sp=100)
+                    m2.stop(stop_action="brake")
+                if 40 < us.value() < 400:
+                    return 1
+            break
+
+    m1.run_forever(speed_sp=250)
+    m2.run_forever(speed_sp=250)
+    time.sleep(0.5)
+    m1.stop(stop_action="brake")
+    m2.stop(stop_action="brake")
+    return 0
+
+def giraRobo(graus, tempo = 2): #90 > 0: direita else: esquerda
+    razaoRobo = 5.5 / 3.0
+    if graus > 0:
+        m1.run_to_rel_pos(position_sp=(razaoRobo*graus),speed_sp=180,stop_action="brake")
+        m2.run_to_rel_pos(position_sp=-(razaoRobo*graus),speed_sp=180,stop_action="brake")
+    else:
+        m1.run_to_rel_pos(position_sp=-(razaoRobo*(graus*-1)),speed_sp=180,stop_action="brake")
+        m2.run_to_rel_pos(position_sp=(zaoRobo*(graus*-1)),speed_sp=180,stop_action="brake")
+    if tempo != 0:
+        time.sleep(tempo)
+
+m1.run_forever(speed_sp=150)
+m2.run_forever(speed_sp=150)
+
+if alinhar(3) == 0:
+    m1.run_to_rel_pos(position_sp=700,speed_sp=150,stop_action="brake")
+    m2.run_to_rel_pos(position_sp=700,speed_sp=150,stop_action="brake")
+    time.sleep(2)
+    giraRobo(-90)       
+'''
+while (us.value() > 230):
+    m1.run_forever(speed_sp=150)
+    m2.run_forever(speed_sp=150)
+    time.sleep(0.3)
+
+m1.stop(stop_action="brake")
+m2.stop(stop_action="brake")
+
+while (us.value() > 230):
+    m1.run_forever(speed_sp=-150)
+    m2.run_forever(speed_sp=-150)
+'''
--- Principal.py
+++ Principal.py
@@ -271,13 +271,13 @@ def AchouCano():
 def scan_gasoduto():
     tempo_inicio, tempos_pista, anterior_leitura = -1, [], 0
     tempo_dez, tempo_quinze, tempo_vinte = 0,0,0
-    #if alinhar(3) == 0:
-    '''m1.run_forever(speed_sp=150)
-    m2.run_forever(speed_sp=150)
-    time.sleep(2)
-    m1.stop(stop_action="brake")
-    m2.stop(stop_action="brake")
-    giraRobo(-90, 3)'''
+    if alinhar(3) == 0:
+        m1.run_forever(speed_sp=150)
+        m2.run_forever(speed_sp=150)
+        time.sleep(2)
+        m1.stop(stop_action="brake")
+        m2.stop(stop_action="brake")
+        giraRobo(-90)
     while True:
         m1.run_forever(speed_sp=150)
         m2.run_forever(speed_sp=150)
