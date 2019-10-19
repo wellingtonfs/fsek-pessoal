48fbc4afaf601af0ad835430c23413c3df2dc7e9
Dyon16 <jardel.dyonisio@hotmail.com>
2019-10-19 20:03:48 -0300

    ola

--- AgoraVai.py
+++ AgoraVai.py
@@ -10,18 +10,18 @@ m2 = LargeMotor('outC')
 m3 = MediumMotor('outB')
 m4 = MediumMotor('outA')
 #us = UltrasonicSensor('in2')
-ir = InfraredSensor('in3')
-ir2 = InfraredSensor('in4')
-Sensor_Cor = [ColorSensor('in1'), ColorSensor('in2')] #1 = Esquerdo, 2 = Direito
+us = UltrasonicSensor('in3')
+us2 = UltrasonicSensor('in4')
+#Sensor_Cor = [ColorSensor('in1'), ColorSensor('in2')] #1 = Esquerdo, 2 = Direito
 
-#us.mode = 'US-DIST-CM'
-ir.mode = 'IR-PROX'
-ir2.mode = 'IR-PROX'
-Sensor_Cor[0].mode = 'COL-COLOR'
-Sensor_Cor[1].mode = 'COL-COLOR'
+us.mode = 'US-DIST-CM'
+us2.mode = 'US-DIST-CM'
+#Sensor_Cor[0].mode = 'COL-COLOR'
+#Sensor_Cor[1].mode = 'COL-COLOR'
 
+'''
 def Encontrar_Pos():
-    while True:#Comm.ir_value
+    while True:
         if Sensor_Cor[0].value() == 1 or Sensor_Cor[1].value() == 1: #Se achar Preto
             Al = alinhar(1)
             if Al == 1:
@@ -34,28 +34,30 @@ def Encontrar_Pos():
                 return 0
         elif Sensor_Cor[0].value() == 3 or Sensor_Cor[1].value() == 3: #Se achar Verde
             Emergencia(180)
-        elif 20 < ir.value() < 42:
+        elif 50 < us.value() < 300:
             print ("Caindo")
-            print (ir.value())
+            print (us.value())
             Emergencia(90)
-        elif 20 < ir2.value() < 42:
+        elif 50 < us2.value() < 300:
             print ("Caindo")
-            print (ir2.value())
+            print (us2.value())
             Emergencia(-90)
 
         m1.run_forever(speed_sp=300)
         m2.run_forever(speed_sp=300)
-
+'''
 def Emergencia(graus):
     m1.stop(stop_action="brake")
     m2.stop(stop_action="brake")
-    m1.run_to_rel_pos(position_sp=-400,speed_sp=200,stop_action="brake")
-    m2.run_to_rel_pos(position_sp=-400,speed_sp=200,stop_action="brake")
+    time.sleep(0.5)
+
+    m1.run_to_rel_pos(position_sp=-400,speed_sp=150,stop_action="brake")
+    m2.run_to_rel_pos(position_sp=-400,speed_sp=150,stop_action="brake")
     time.sleep(3)
     giraRobo(graus)
     m1.run_forever(speed_sp=300)
     m2.run_forever(speed_sp=300)
-
+'''
 def gyro(an):
     baseAngle = gy.value()
 
@@ -70,6 +72,8 @@ def gyro(an):
     else:
         mdiff.turn_right(SpeedRPM(40), abs(diffAng))
     time.sleep(2)
+'''
+
 
 def giraRobo(graus, tempo = 2): #90 > 0: direita else: esquerda
     razaoRobo = 5.25 / 3.0
@@ -86,7 +90,7 @@ def giraRobo(graus, tempo = 2): #90 > 0: direita else: esquerda
         m2.run_to_rel_pos(position_sp=(razaoRobo*(graus*-1)),speed_sp=280,stop_action="brake")
     if tempo != 0:
         time.sleep(tempo)
-
+'''
 def alinhar(c): #Essa função alinha o lego a uma cor especifica c.
     if Sensor_Cor[0].value() == c and Sensor_Cor[1].value() == c:
         return 0
@@ -110,7 +114,7 @@ def alinhar(c): #Essa função alinha o lego a uma cor especifica c.
                 else:
                     m1.stop(stop_action="brake")
                     m2.run_forever(speed_sp=100)
-                if 20 < ir2.value() < 42:
+                if 50 < us2.value() < 300:
                     return 2
             break
 
@@ -133,7 +137,7 @@ def alinhar(c): #Essa função alinha o lego a uma cor especifica c.
                 else:
                     m1.run_forever(speed_sp=100)
                     m2.stop(stop_action="brake")
-                if 20 < ir.value() < 42:
+                if 50 < us.value() < 300:
                     return 1
             break
 
@@ -142,7 +146,11 @@ def alinhar(c): #Essa função alinha o lego a uma cor especifica c.
     time.sleep(1)
     m1.stop(stop_action="brake")
     m2.stop(stop_action="brake")
-    return 0
+    return 0'''
 
+while True:
+    m1.run_forever(speed_sp=150)
+    m2.run_forever(speed_sp=150)
 
-Encontrar_Pos()
+    if (us.value() >= 50) or (us2.value() >= 50):
+        Emergencia(90)
